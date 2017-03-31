0a6ef631789b78080499b65cc9cc38238b34d9f0
Roman Mashkovskyy <romjulia75@gmail.com>
2017-04-01 00:09:01 +0200

    no message

---
+++ w9_SI_MSMQ-sender-translator-receiver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w9_SI_MSMQ_sender_translator_receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
